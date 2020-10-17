9461b36602ec02a6500697eaf9e33e43daca8e39
jkarshen <70391243+jkarshen@users.noreply.github.com>
2020-10-17 13:04:20 -0400

    Add files via upload
    The main bulk of our code

---
+++ Mask On/Mask On/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mask On
+//
+//  Created by Jackson Karshen on 10/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
