0e70d2617def5dbe24570e69986b6f221738470d
Harjas Monga <harjasmonga@gmail.com>
2020-10-17 23:11:39 -0400

    redid menu controller and navigation

--- Mask On/Mask On/AppDelegate.swift
+++ Mask On/Mask On/AppDelegate.swift
@@ -14,9 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow()
-        window?.makeKeyAndVisible()
-        window?.rootViewController = ContainerController()
+//        window = UIWindow()
+//        window?.makeKeyAndVisible()
+//        window?.rootViewController = ContainerController()
         
         return true
     }
